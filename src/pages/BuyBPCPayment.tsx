87332607e08cac2bb969b230b17cd7f6481a5a4a
Avoidseefinish <emmanuelstone1240@gmail.com>
2025-08-17 15:08:42 +0100

    Update BuyBPCPayment.tsx

--- src/pages/BuyBPCPayment.tsx
+++ src/pages/BuyBPCPayment.tsx
@@ -111,12 +111,12 @@ const BuyBPCPayment = () => {
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Number</p>
           <div className="flex justify-between items-center">
-            <p className="text-lg font-bold">5131637793</p>
+            <p className="text-lg font-bold">1100082653</p>
             <Button
               variant="default"
               size="sm"
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-1"
-              onClick={() => handleCopy("5131637793", "Account Number")}
+              onClick={() => handleCopy("1100082653", "Account Number")}
             >
               <Copy size={14} />
               Copy
@@ -126,12 +126,12 @@ const BuyBPCPayment = () => {
 
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Bank Name</p>
-          <p className="text-lg font-bold">CREDIT DIRECT LIMITED</p>
+          <p className="text-lg font-bold">EMAAR MICROFINANCE BANK LIMITED</p>
         </div>
 
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Name</p>
-          <p className="text-lg font-bold">USMAN KATUN MOHAMMED</p>
+          <p className="text-lg font-bold">MOHAMMED KATUN</p>
         </div>
       </div>
 
